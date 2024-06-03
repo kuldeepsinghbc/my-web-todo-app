cf1f04d8cac4f2b1fb3a436ea727fe87e9398657
Kuldeep Singh <kuldeephimelf@gmail.com>
2024-06-03 09:12:03 -0700

    Change code blocks in Code Experiments

--- web.py
+++ web.py
@@ -3,7 +3,7 @@ import functions
 
 todos = functions.get_todos()
 
-
+st.set_page_config(layout="wide")
 def add_todo():
     todo = st.session_state["new_todo"] + "\n"
     todos.append(todo)
@@ -12,7 +12,13 @@ def add_todo():
 
 st.title("My Todo App")
 st.subheader("This is my todo app.")
-st.write("This app is to increase your productivity.")
+st.write("This app is to increase your <b>productivity</b>.",
+         unsafe_allow_html=True)
+
+# st.text_input(label="Enter a Todo")
+st.text_input(label="", placeholder="Add a new todo...",
+              on_change=add_todo, key='new_todo')
+
 
 for index, todo in enumerate(todos):
     checkbox = st.checkbox(todo, key=todo)
@@ -22,8 +28,6 @@ for index, todo in enumerate(todos):
         del st.session_state[todo]
         st.experimental_rerun()
 
-# st.text_input(label="Enter a Todo")
-st.text_input(label="", placeholder="Add a new todo...",
-              on_change=add_todo, key='new_todo')
+
 
 # st.session_state
